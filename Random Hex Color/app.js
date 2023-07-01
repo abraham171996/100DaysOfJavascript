732377d2fdb7d8cf598034e4cc91388e50790356
abraham171996 <ibrahimmemmed3131908@gmail.com>
2023-07-02 02:13:42 +0400

    feat:add new task name is multi step form day 21

--- Random Hex Color/app.js
+++ Random Hex Color/app.js
@@ -1,10 +1,18 @@
 const hex = document.querySelector(".hex");
 const btn = document.querySelector(".generate");
-
+const copy = document.querySelector(".copy")
+const text = document.querySelector("p")
 const generateColor = () => {
     const randomColor = Math.random().toString(16).slice(2,8);
     document.body.style.backgroundColor = "#" + randomColor;
     hex.innerHTML = "#" + randomColor;
+    text.textContent = ""
 }
 
 btn.addEventListener("click",generateColor);
+
+const copyText = ()=>{
+    navigator.clipboard.writeText(hex.textContent);
+    text.textContent =`The hex code of the color has been copied`
+}
+copy.addEventListener('click',copyText)
